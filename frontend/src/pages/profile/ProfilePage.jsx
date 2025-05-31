7e0932f71a00f98820960eda37f287773647dea3
Vijaya Mishra <134155725+Vijayaa21@users.noreply.github.com>
2025-05-31 18:23:39 +0530

    auth handles and routes proteched in client

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -12,6 +12,7 @@ import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
 
+
 const ProfilePage = () => {
 	const [coverImg, setCoverImg] = useState(null);
 	const [profileImg, setProfileImg] = useState(null);
@@ -91,7 +92,7 @@ const ProfilePage = () => {
 								<input
 									type='file'
 									hidden
-									accept="image/*"
+									
 									ref={profileImgRef}
 									onChange={(e) => handleImgChange(e, "profileImg")}
 								/>
