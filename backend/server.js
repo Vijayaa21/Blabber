a9dea952d0eb6a84b32c542553113f61aa2c462d
Vijaya Mishra <134155725+Vijayaa21@users.noreply.github.com>
2025-06-05 18:09:08 +0530

    frontend backend combined

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import notificationRoutes from "./routes/notification.route.js";
 import dotenv from"dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
+
+
 
 dotenv.config()
 
@@ -14,6 +17,11 @@ const app =  express();
 const PORT = process.env.PORT || 5000
 
 console.log(process.env.MONGO_URI)
+
+app.use(cors({
+  origin: 'https://blabber-front.onrender.com/',
+  credentials: true
+}));
 
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ extended:true }));
@@ -28,6 +36,9 @@ app.use("/api/notifications", notificationRoutes);
 app.get("/", (req, res) => {
     res.send("Welcome to the Blabber server");
 });   
+
+
+
 
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
