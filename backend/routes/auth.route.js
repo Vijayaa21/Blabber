66a5fbb41a83256a1b6685f6e1e2b3473cd43b28
Soham Lodh <sohamlodh06@gmail.com>
2025-08-06 02:50:27 +0530

    Updated Auth Features

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,14 +1,29 @@
-import express from "express"
-import {signup, login, logout, getMe} from "../controllers/auth.controller.js"
-import { protectRoute } from "../middleware/protectRoute.js"
+import express from "express";
+import {
+  signup,
+  login,
+  logout,
+  getMe,
+  sendVerifyOtp,
+  verifyOtp,
+  sendResetOtp,
+  resetPassword,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/me", protectRoute, getMe)
-router.post("/signup", signup)
 
-router.post("/login", login)
+router.get("/me", protectRoute, getMe);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post("/logout", logout)
+
+router.post("/send-verify-otp", protectRoute, sendVerifyOtp); // send OTP after signup
+router.post("/verify-otp", protectRoute, verifyOtp);          // verify account using OTP
+
+router.post("/send-reset-otp", sendResetOtp);                 // send OTP to email for password reset
+router.post("/reset-password", resetPassword);                // reset password using OTP
 
 export default router;
