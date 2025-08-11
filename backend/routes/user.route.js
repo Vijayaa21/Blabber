d29970715a6eb6d955ab699d9e6e3e7d44057c77
rishika638 <rishika82871@gmail.com>
2025-08-11 09:58:53 +0530

    files committed for view followers following

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";  
-import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUserProfile } from "../controllers/user.controllers.js";
+import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUserProfile, getFollowers,getFollowing } from "../controllers/user.controllers.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
@@ -8,7 +8,7 @@ router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 router.post("/update",protectRoute, updateUserProfile);
-
-
+router.get('/:id/followers', getFollowers);
+router.get('/:id/following', getFollowing);
 
 export default router;
