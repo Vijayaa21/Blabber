dfd46da11988a72417f5b23fa73c473920a856a1
ShubhamDidharia <shubhamdidharia627@gmail.com>
2025-08-19 14:27:10 +0530

    issue #51 fixed

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -5,8 +5,11 @@ const connectMongoDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`);
+        console.log(process.env.MONGO_URI);
+        console.log(process.env.PORT);
     } catch(error){
         console.error(`Error connection to mongoDB: ${error.message}`);
+        console.log(process.env.MONGO_URI);
         process.exit(1);
     }
 }
