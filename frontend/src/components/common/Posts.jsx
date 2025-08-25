69e88d8e91afe7ea674b778e778aeefb1a3fefac
msuudoct2@gmail.com <msuudoct2@gmail.com>
2025-08-25 22:12:29 +0530

    fixed the toest error message while likning a post

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,25 +1,23 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
 
-const Posts = ({ posts, isLoading, isRefetching }) => {
+const Posts = ({ posts, isLoading, isRefetching, feedType }) => {
   return (
     <>
-      {(isLoading || isRefetching) && (
+      {isLoading && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && !isRefetching && posts?.length === 0 && (
+      {!isLoading && posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && !isRefetching && posts && (
+      {!isLoading && posts && (
         <div>
           {posts.map((post) => (
-            <Post key={post._id} post={post} />
+            <Post key={post._id} post={post} feedType={feedType}/>
           ))}
         </div>
       )}
